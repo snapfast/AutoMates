7b74a88ea4866ad80c9c57000b8a4d993a0f147e
rahbal <rahulrdb18@gmail.com>
2022-08-03 21:39:56 +0530

    done

--- LINKEDINCOM/search_jobs.py
+++ LINKEDINCOM/search_jobs.py
@@ -55,7 +55,7 @@ class LinkedinBot():
             by=By.TAG_NAME, value='button').click()
 
         print(easy_apply_filter_parent.text)
-        sleep(2)
+        sleep(10)
 
         # left panel jobs
         try:
@@ -63,34 +63,44 @@ class LinkedinBot():
                 by=By.CLASS_NAME, value="jobs-search-results__list-item")
         except NoSuchElementException as NoSuch:
             print(NoSuch, "\n cool")
-        print(len(left_panel_jobs))
+
+        total_jobs = len(left_panel_jobs)
+        print(total_jobs, left_panel_jobs)
 
         # storing the main tab context
         original_window = self.driver.current_window_handle
-        jj = 0
+        j = 0
 
         # looping on each left panel job one by one.
-        while 1:
+        while j < total_jobs:
             sleep(2)
-            one_job = left_panel_jobs[jj]
             try:
-                job_name_element = one_job.find_element(
-                    by=By.CLASS_NAME, value='job-card-list__title')
+                job_name_element = left_panel_jobs[j].find_element(
+                    by=By.CLASS_NAME,
+                    value='job-card-list__title')
+                company_name_element = left_panel_jobs[j].find_element(
+                    by=By.CLASS_NAME,
+                    value='job-card-container__company-name')
+                location_element = left_panel_jobs[j].find_element(
+                    by=By.CLASS_NAME,
+                    value='job-card-container__metadata-wrapper')
                 jname = job_name_element.text
+                cname = company_name_element.text
+                lname = location_element.text
                 job_url = job_name_element.get_attribute('href')
-                print(jname, job_url)
+                print(jname + "--" + cname + "--" + lname)
 
                 with open(r'filename.txt', 'a') as f:
                     print(jname, file=f)
                 self.driver.execute_script(
-                    "arguments[0].scrollIntoView(true);", one_job)
+                    "arguments[0].scrollIntoView(true);", left_panel_jobs[j])
                 self.driver.switch_to.new_window('tab')
                 self.driver._switch_to.window(self.driver.window_handles[1])
                 self.driver.get(job_url)
                 sleep(2)
                 self.apply_job()
                 self.driver._switch_to.window(original_window)
-                jj += 1
+                j += 1
             except ElementNotInteractableException:
                 print('scroll a bit please, cannot see the element yet')
                 sleep(2)
@@ -115,15 +125,15 @@ class LinkedinBot():
         gerat = True
         cc = 5
         while gerat:
-            try:
-                checkbox = self.driver.find_element(
-                    by=By.CLASS_NAME, value="ember-checkbox")
-                self.driver.execute_script(
-                    "arguments[0].scrollIntoView(true);", checkbox)
-                checkbox.click()
-                print("clicked checkbox")
-            except Exception as e:
-                print(e, "no checkbox")
+            # try:
+            #     checkbox = self.driver.find_element(
+            #         by=By.CLASS_NAME, value="ember-checkbox")
+            #     self.driver.execute_script(
+            #         "arguments[0].scrollIntoView(true);", checkbox)
+            #     checkbox.click()
+            #     print("clicked checkbox")
+            # except Exception as e:
+            #     print(e, "no checkbox")
             try:
                 self.driver.find_element(by=By.XPATH,
                     value="//button[@class='artdeco-button artdeco-button--2 artdeco-button--primary ember-view']").click()
