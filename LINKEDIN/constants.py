89ded19c4a8990ca2e5b67bc590b48c269b2949b
harsh52 <hkumar8897@gmail.com>
2024-05-30 21:08:40 +0530

    added support for linkedin recruiter post, based on hiring positions

---
+++ LINKEDIN/constants.py
@@ -0,0 +1,7 @@
+LINKEDIN_CANDIDATE_INFO = """
+Hi,
+I have a total of 4+ years of experience as full stack engineer. 
+Build/scale SaaS products from scratch and contributed to many great products.  
+Resume: drive.google.com/file/d/10MltbCkbsKsDQVq56MBeHBwl99fPqsMu/view?usp=drive_link
+Kindly let me know if there are any requirements.
+"""
